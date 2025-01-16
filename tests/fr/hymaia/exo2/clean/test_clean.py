fa9c571678f5ee2c4287e7146cda01a6b48912ce
opixelum <48217573+opixelum@users.noreply.github.com>
2025-01-16 20:10:09 +0100

    refactor(test_clean.py): change some zip codes

--- tests/fr/hymaia/exo2/clean/test_clean.py
+++ tests/fr/hymaia/exo2/clean/test_clean.py
@@ -9,15 +9,15 @@ class TestClean(unittest.TestCase):
     def test_only_adults(self):
         # Given
         data: DataFrame = spark.createDataFrame([
-            Row(name="John", age=67, zip=10001),
-            Row(name="Jane", age=46, zip=20000),
-            Row(name="James", age=16, zip=20600),
-            Row(name="Jack", age=24, zip=20600)
+            Row(name="John", age=67, zip="04001"),
+            Row(name="Jane", age=46, zip="20000"),
+            Row(name="James", age=16, zip="20600"),
+            Row(name="Jack", age=24, zip="20600")
         ])
         expected: DataFrame = spark.createDataFrame([
-            Row(name="John", age=67, zip=10001),
-            Row(name="Jane", age=46, zip=20000),
-            Row(name="Jack", age=24, zip=20600)
+            Row(name="John", age=67, zip="04001"),
+            Row(name="Jane", age=46, zip="20000"),
+            Row(name="Jack", age=24, zip="20600")
         ])
 
         # When
@@ -29,19 +29,19 @@ class TestClean(unittest.TestCase):
     def test_join_clients_cities(self):
         # Given
         clients_data: DataFrame = spark.createDataFrame([
-            Row(name="John", age=67, zip=10001),
-            Row(name="Jane", age=46, zip=20000),
-            Row(name="Jack", age=24, zip=20600)
+            Row(name="John", age=67, zip="04001"),
+            Row(name="Jane", age=46, zip="20000"),
+            Row(name="Jack", age=24, zip="20600")
         ])
         cities_data: DataFrame = spark.createDataFrame([
-            Row(zip=10001, city="New York"),
-            Row(zip=20000, city="Ajaccio"),
-            Row(zip=20600, city="Bastia"),
+            Row(zip="04001", city="New York"),
+            Row(zip="20000", city="Ajaccio"),
+            Row(zip="20600", city="Bastia"),
         ])
         expected: DataFrame = spark.createDataFrame([
-            Row(name="John", age=67, zip=10001, city="New York"),
-            Row(name="Jane", age=46, zip=20000, city="Ajaccio"),
-            Row(name="Jack", age=24, zip=20600, city="Bastia")
+            Row(name="John", age=67, zip="04001", city="New York"),
+            Row(name="Jane", age=46, zip="20000", city="Ajaccio"),
+            Row(name="Jack", age=24, zip="20600", city="Bastia")
         ])
 
         # When
@@ -53,14 +53,14 @@ class TestClean(unittest.TestCase):
     def test_add_department_column(self):
         # Given
         data: DataFrame = spark.createDataFrame([
-            Row(name="John", age=67, zip=10001, city="New York"),
-            Row(name="Jane", age=46, zip=20000, city="Ajaccio"),
-            Row(name="Jack", age=24, zip=20600, city="Bastia")
+            Row(name="John", age=67, zip="04001", city="New York"),
+            Row(name="Jane", age=46, zip="20000", city="Ajaccio"),
+            Row(name="Jack", age=24, zip="20600", city="Bastia")
         ])
         expected: DataFrame = spark.createDataFrame([
-            Row(name="John", age=67, zip=10001, city="New York", department="10"),
-            Row(name="Jane", age=46, zip=20000, city="Ajaccio", department="2A"),
-            Row(name="Jack", age=24, zip=20600, city="Bastia", department="2B")
+            Row(name="John", age=67, zip="04001", city="New York", department="04"),
+            Row(name="Jane", age=46, zip="20000", city="Ajaccio", department="2A"),
+            Row(name="Jack", age=24, zip="20600", city="Bastia", department="2B")
         ])
 
         # When
@@ -72,21 +72,24 @@ class TestClean(unittest.TestCase):
     def test_integration(self):
         # Given
         clients_df: DataFrame = spark.createDataFrame([
-            Row(name="John", age=67, zip=10001),
-            Row(name="Jane", age=46, zip=20000),
-            Row(name="James", age=16, zip=20600),
-            Row(name="Jack", age=24, zip=20600)
+            Row(name="John", age=67, zip="04001"),
+            Row(name="Jane", age=46, zip="20000"),
+            Row(name="James", age=16, zip="20600"),
+            Row(name="Jack", age=24, zip="20600")
         ])
         cities_df: DataFrame = spark.createDataFrame([
-            Row(zip=10001, city="New York"),
-            Row(zip=20000, city="Ajaccio"),
-            Row(zip=20600, city="Bastia"),
+            Row(zip="04001", city="New York"),
+            Row(zip="20000", city="Ajaccio"),
+            Row(zip="20600", city="Bastia"),
         ])
         expected: DataFrame = spark.createDataFrame([
-            Row(name="John", age=67, zip=10001, city="New York", department="10"),
-            Row(name="Jane", age=46, zip=20000, city="Ajaccio", department="2A"),
-            Row(name="Jack", age=24, zip=20600, city="Bastia", department="2B")
+            Row(name="John", age=67, zip="10001", city="New York", department="04"),
+            Row(name="Jane", age=46, zip="20000", city="Ajaccio", department="2A"),
+            Row(name="Jack", age=24, zip="20600", city="Bastia", department="2B")
         ])
+
+        # With assert(valueerror):
+            # clean(...) # ask margot
 
         # When
         actual: DataFrame = clean(clients_df, cities_df)
